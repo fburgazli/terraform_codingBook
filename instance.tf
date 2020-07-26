046deae4acac579dfb23d4a96862e8e8c61d3207
Fikret Burgazli <fburgazli@hotmail.com>
2020-07-26 17:20:14 -0400

    keypair added

--- instance.tf
+++ instance.tf
@@ -6,7 +6,7 @@ provider "aws" {
 resource "aws_instance" "myEC2" {
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
-  key_name = "fikretLaptop"
+  key_name = "codingBookKey"
   subnet_id = aws_subnet.public_subnet.id
   associate_public_ip_address = true
 
@@ -15,4 +15,18 @@ resource "aws_instance" "myEC2" {
     Name = "web_server"
   }
   
+  connection {
+    type = "ssh"
+    user = "ubuntu"
+    private_key = file("~/.ssh/id_rsa")
+    host = self.public_ip
+ }
+  provisioner "remote-exec" {
+     inline = [
+        "sudo apt update",
+        "sudo apt install nginx -y",
+        "sudo systemctl start nginx",
+        "sudo systemctl enable nginx"
+   ]
+ }
 }
