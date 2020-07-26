89d77237f7d6e51f6cdf4b6fe934dd753010a83c
Fikret Burgazli <fburgazli@hotmail.com>
2020-07-26 16:06:19 -0400

    terraform resources

--- instance.tf
+++ instance.tf
@@ -3,12 +3,16 @@ provider "aws" {
   version = "~> 2.70"
   region  = "us-east-1"
 }
-resource "aws_instance" "my-EC2" {
+resource "aws_instance" "myEC2" {
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
+  key_name = "fikretLaptop"
+  subnet_id = aws_subnet.public_subnet.id
+  associate_public_ip_address = true
+
 
   tags = {
-      Name = "web_server"
+    Name = "web_server"
   }
-  depends_on = [aws_vpc.main]
+  
 }
